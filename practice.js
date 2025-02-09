50160b34f3a99e7deb7054e41010b76f8f524a67
grypis <grypisnecros@gmail.com>
2025-02-09 22:48:27 +0200

    asd

--- practice.js
+++ practice.js
@@ -2,9 +2,33 @@ function greet() {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('Hello world!');
-    }, 1000);
+    }, 2000);
   });
   return promise;
 }
 
-console.log(greet());
+/* greet().then(data => {
+  console.log(data);
+}); */
+
+greet().then(callback);
+
+function callback(data) {
+  console.log(data);
+}
+
+//! ====================================
+const data = Number(prompt('Введіть число: '));
+checkNumber(data).then(console.log).catch(console.log);
+
+function checkNumber(data) {
+  return new Promise((resolve, reject) => {
+    if (!data) {
+      reject('error');
+    } else if (data % 2 === 0) {
+      setTimeout(() => resolve('even'), 1000);
+    } else {
+      setTimeout(() => resolve('odd'), 2000);
+    }
+  });
+}
